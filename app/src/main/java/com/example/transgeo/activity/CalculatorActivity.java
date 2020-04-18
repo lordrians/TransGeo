324bdc951ceca23fa09abf23d46b8ef646a839f5
ferrian <ferrianrp@gmail.com>
2020-04-18 07:30:26 +0700

    calculator beres

--- app/src/main/java/com/example/transgeo/activity/CalculatorActivity.java
+++ app/src/main/java/com/example/transgeo/activity/CalculatorActivity.java
@@ -2,18 +2,31 @@ package com.example.transgeo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+//
+//import org.mozilla.javascript.Context;
+//import org.mozilla.javascript.Evaluator;
+//import org.mozilla.javascript.Scriptable;
 
 import com.example.transgeo.R;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
     Button btnPlusMin, btnKoma, btnEqual, btnTambah, btnKurang, btnKali, btnBagi, btnPersen, btnKurung, btnClear;
     ImageButton ivBackspace;
+    TextView tvInput, tvOutput;
+    String proses = "";
+    Boolean cekKurung = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +56,34 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         btnKurung = findViewById(R.id.btn_cal_kurung);
         btnClear = findViewById(R.id.btn_cal_clear);
 
+        tvInput = findViewById(R.id.tv_cal_input);
+        tvOutput = findViewById(R.id.tv_cal_output);
+
         ivBackspace = findViewById(R.id.iv_cal_backspace);
 
 
         btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+        btnKoma.setOnClickListener(this);
+        btnKurang.setOnClickListener(this);
+        btnKurung.setOnClickListener(this);
+        btnPersen.setOnClickListener(this);
+        btnBagi.setOnClickListener(this);
+        btnKali.setOnClickListener(this);
+        btnTambah.setOnClickListener(this);
+        btnEqual.setOnClickListener(this);
+        btnPlusMin.setOnClickListener(this);
+
+        ivBackspace.setOnClickListener(this);
 
 
     }
@@ -57,42 +94,59 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         switch (view.getId()){
 
             case R.id.btn_cal_0:
+//                if (tvOutput.getText().toString().isEmpty()){
+//                    proses = tvInput.getText().toString();
+//                    tvInput.setText(proses + "0");
+//                } else {
+//                    clearInOut();
+//                    proses = tvInput.getText().toString();
+//                    tvInput.setText(proses + "0");
+//                }
+                appendOnExpresstion("0", true);
 
                 break;
 
             case R.id.btn_cal_1:
-
+                appendOnExpresstion("1", true);
                 break;
 
             case R.id.btn_cal_2:
+                appendOnExpresstion("2", true);
 
                 break;
 
             case R.id.btn_cal_3:
+                appendOnExpresstion("3", true);
 
                 break;
 
             case R.id.btn_cal_4:
+                appendOnExpresstion("4", true);
 
                 break;
 
             case R.id.btn_cal_5 :
+                appendOnExpresstion("5", true);
 
                 break;
 
             case R.id.btn_cal_6:
+                appendOnExpresstion("6", true);
 
                 break;
 
             case R.id.btn_cal_7:
+                appendOnExpresstion("7", true);
 
                 break;
 
             case R.id.btn_cal_8:
+                appendOnExpresstion("8", true);
 
                 break;
 
             case R.id.btn_cal_9:
+                appendOnExpresstion("9", true);
 
                 break;
 
@@ -101,47 +155,123 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
                 break;
 
             case R.id.btn_cal_koma:
-
+                appendOnExpresstion(".", true);
                 break;
 
-            case R.id.btn_cal_equal:
-
-                break;
 
             case R.id.btn_cal_perjumlahan:
-
+                appendOnExpresstion("+", false);
                 break;
 
             case R.id.btn_cal_pengurangan:
-
+                appendOnExpresstion("-", false);
                 break;
 
             case R.id.btn_cal_pembagian:
-
+                appendOnExpresstion("/", false);
                 break;
 
             case R.id.btn_cal_perkalian:
-
+                appendOnExpresstion("*", false);
                 break;
 
             case R.id.btn_cal_persen:
-
+                appendOnExpresstion("/100", false);
                 break;
 
             case R.id.btn_cal_kurung:
+                if (cekKurung){
+//                    proses = tvInput.getText().toString();
+//                    tvInput.setText(proses + ")");
+                    appendOnExpresstion(")", false);
+                    cekKurung = false;
+                } else {
+//                    proses = tvInput.getText().toString();
+//                    tvInput.setText(proses + "(");
+                    appendOnExpresstion("(", false);
+
+                    cekKurung = true;
+                }
 
                 break;
 
             case R.id.btn_cal_clear:
-
+                clearInOut();
                 break;
 
             case R.id.iv_cal_backspace:
-
+                proses = tvInput.getText().toString();
+                if (!proses.isEmpty()){
+                    tvInput.setText(proses.substring(0,proses.length()-1));
+                } else {
+                    tvInput.setText("");
+                }
                 break;
+
+                case R.id.btn_cal_equal:
+                    try {
+                        Expression expressionBuilder = new ExpressionBuilder(tvInput.getText().toString()).build();
+                        Long hasilLong ;
+                        double result = expressionBuilder.evaluate();
+                        hasilLong = (new Double(result).longValue());
+                        if (result == hasilLong.doubleValue()){
+                            tvOutput.setText(hasilLong.toString());
+                        } else {
+                            tvOutput.setText(String.valueOf(result));
+                        }
+                    } catch (Exception e){
+                        Toast.makeText(getApplicationContext(), e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+//            case R.id.btn_cal_equal:
+//                proses = tvInput.getText().toString();
+//
+//                proses = proses.replaceAll("x", "*");
+//                proses = proses.replaceAll("%", "/100");
+//
+//                Context rhino = Context.enter();
+//
+//                rhino.setOptimizationLevel(-1);
+//
+//                String finalResult = "";
+//
+//                try {
+//                    Scriptable scriptable = rhino.initStandardObjects();
+//                    finalResult = rhino.evaluateString(scriptable, proses, "javascript", 1, null).toString();
+//                }catch (Exception e){
+//                    finalResult = "0";
+//                }
+//                tvOutput.setText(finalResult);
+//
+//                break;
 
 
         }
 
+    }
+
+    private void appendOnExpresstion(String string, Boolean canClear) {
+        tvOutput = findViewById(R.id.tv_cal_output);
+        tvInput = findViewById(R.id.tv_cal_input);
+
+        if (!tvOutput.getText().equals("")){
+            tvInput.setText("");
+        }
+
+        if (canClear){
+            tvOutput.setText("");
+            tvInput.append(string);
+        } else {
+            tvInput.append(tvOutput.getText());
+            tvInput.append(string);
+            tvOutput.setText("");
+        }
+    }
+
+    private void clearInOut() {
+        tvOutput.setText("");
+        tvInput.setText("");
     }
 }
